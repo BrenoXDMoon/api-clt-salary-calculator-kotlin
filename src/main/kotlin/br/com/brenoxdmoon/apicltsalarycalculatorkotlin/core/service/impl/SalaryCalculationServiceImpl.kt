277ecdc8e21df3b614ec9ebd539fdd1250f9d3dc
Breno Gabriel <brenogabriel19@gmail.com>
2025-03-19 12:10:41 -0300

    Ajuste no cálculo de salário e correção de bug de arredondamento

--- src/main/kotlin/br/com/brenoxdmoon/apicltsalarycalculatorkotlin/core/service/impl/SalaryCalculationServiceImpl.kt
+++ src/main/kotlin/br/com/brenoxdmoon/apicltsalarycalculatorkotlin/core/service/impl/SalaryCalculationServiceImpl.kt
@@ -6,36 +6,39 @@ import br.com.brenoxdmoon.apicltsalarycalculatorkotlin.inbound.dto.SalaryCalcula
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
+import java.math.MathContext
 
 @Service
 class SalaryCalculationServiceImpl : SalaryCalculationService {
 
-    @Value("\${inss}")
-    private lateinit var inssPercentage: BigDecimal
-
-    @Value("\${ir}")
-    private lateinit var irPercentage: BigDecimal
 
     @Value("\${deduction-by-dependent}")
     private lateinit var dependentDeductionAmount: BigDecimal
 
     override fun calculateSalary(calculationDataDTO: CalculationDataDTO): SalaryCalculatedDTO {
 
-        // Cálculo do INSS
-        val inss = calculationDataDTO.grossSalary * inssPercentage
+        val inss = when {
+            calculationDataDTO.grossSalary <= BigDecimal("1212.00") -> calculationDataDTO.grossSalary * BigDecimal("0.075")
+            calculationDataDTO.grossSalary <= BigDecimal("2427.35") -> calculationDataDTO.grossSalary * BigDecimal("0.09")
+            calculationDataDTO.grossSalary <= BigDecimal("3641.03") -> calculationDataDTO.grossSalary * BigDecimal("0.12")
+            calculationDataDTO.grossSalary <= BigDecimal("7087.22") -> calculationDataDTO.grossSalary * BigDecimal("0.14")
+            else -> BigDecimal("828.39")
+        }
 
-        // Cálculo da base de cálculo do IR
         val irBase = calculationDataDTO.grossSalary - inss - calculationDataDTO.discounts
 
-        // Cálculo do desconto do IR
-        val ir = if (irBase > BigDecimal.ZERO) irBase * irPercentage else BigDecimal.ZERO
+        val ir = when {
+            irBase <= BigDecimal("1903.98") -> BigDecimal.ZERO
+            irBase <= BigDecimal("2826.65") -> irBase * BigDecimal("0.075") - BigDecimal("142.80")
+            irBase <= BigDecimal("3751.05") -> irBase * BigDecimal("0.15") - BigDecimal("354.80")
+            irBase <= BigDecimal("4664.68") -> irBase * BigDecimal("0.225") - BigDecimal("636.13")
+            else -> irBase * BigDecimal("0.275") - BigDecimal("869.36")
+        }
 
-        // Cálculo do desconto por dependentes
         val dependentDeductionTotal = dependentDeductionAmount * BigDecimal(calculationDataDTO.numberOfDependents)
 
-        // Salário líquido
-        val netSalary = calculationDataDTO.grossSalary - inss - ir + calculationDataDTO.discounts - dependentDeductionTotal
+        val netSalary = calculationDataDTO.grossSalary - (ir + inss + calculationDataDTO.discounts + dependentDeductionTotal)
 
-        return SalaryCalculatedDTO(netSalary.setScale(2))
+        return SalaryCalculatedDTO(netSalary.setScale(2, BigDecimal.ROUND_HALF_UP))
     }
 }
